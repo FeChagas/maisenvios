18d6b68c4a0e37fc47fc1b3f4cdb832c80739acb
Felipe <fechagas.dev@gmail.com>
2022-03-10 19:13:22 -0300

    added logs

--- backend/src/Service/VtexService.php
+++ backend/src/Service/VtexService.php
@@ -2,24 +2,30 @@
 namespace Maisenvios\Middleware\Service;
 
 use Maisenvios\Middleware\Client\Vtex;
+use Maisenvios\Middleware\Model\SgpLog;
 use Maisenvios\Middleware\Model\Shop;
+use Maisenvios\Middleware\Repository\SgpLogRepository;
 
 class VtexService {
 
     private $vtexClient;
     private $shop;
+    private $sgpLogRepo;
 
     public function __construct(Shop $shop)
     {
+        $this->sgpLogRepo = new SgpLogRepository();
         if (strcmp($shop->getEcommerce(), 'VTEX') !== 0) {
-            //gravar log de erro
-            throw new \Exception("Shop must by VTEX", 1);            
-        }
+            throw new \Exception("Trying to use an non-VTEX shop on VtexService", 1);            
+        }        
         
         if ($shop->getAccount() !== null && $shop->getCustomerKey() !== null && $shop->getCustomerToken() !== null) {
             $this->vtexClient = new Vtex($shop->getAccount(), $shop->getCustomerKey(), $shop->getCustomerToken());
         } else {
-            //Grava log de erro
+            $log = new SgpLog();
+            $log->setShopId( $shop->getId() );
+            $log->setStatus("As informações de Account, CustomerKey, CustomerToken não estão disponiveis e são necessárias.");
+            $this->sgpLogRepo->create($log);
             throw new \Exception("Shop must have account, key and token", 1);            
         }
 
@@ -53,6 +59,11 @@ class VtexService {
                 ]
             ];
             $this->vtexClient->createHook($hookArgs);
+            $log = new SgpLog();
+            $log->setShopId( $this->shop->getId() );
+            $log->setStatus("Hook criado.");
+            $log->setObjetos(json_encode($hookArgs));
+            $this->sgpLogRepo->create($log);
         }
         return;
     }
