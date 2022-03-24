8d900510a8d05eee91035f73498fda21c23ad1f6
Felipe <fechagas.dev@gmail.com>
2022-03-23 21:59:46 -0300

    new shop bug fix

--- php/shop/new.php
+++ php/shop/new.php
@@ -3,7 +3,6 @@
   include '../connection/mysql.php'; 
 ?>
 <?php
-  $retorno = [];
   $name = $_POST['name'];
   $key_mais = $_POST['key_mais'];
   $key_primary = $_POST['key_primary'];
@@ -12,11 +11,7 @@
   $ecommerce = $_POST['ecommerce'];
   
   $insert = "INSERT INTO `shop` (`id`, `name`, `key_mais`, `key_primary`, `token_primary`, `account`, `ecommerce`, `active`) VALUES (NULL, '$name', '$key_mais', '$key_primary', '$token_primary', '$account', '$ecommerce', '1');";
-  $result = $link->query($insert);
-  array_push($retorno, array(
-    "status" => 0,
-    "message" => 'Cadastro realizado com sucesso'
-  ));
-      
-  echo json_encode($retorno);
+  echo json_encode([
+    'success' => $link->query($insert)
+]);
 ?>
